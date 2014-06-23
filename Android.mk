5a2d983c272f46b2491649fe3e5752f64aa56bcf
DeanHuangTW <DeanHuang@via.com.tw>
2014-06-23 09:21:28 +0800

    Replace to wpa_supplicant
    Vixen only use rtl8188e wifi chip
    So we replace it directly

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS) 
 
-LOCAL_SRC_FILES += wpa_supplicant_rtl8192e:system/bin/wpa_supplicant_rtl8192e
-LOCAL_SRC_FILES += hostapd_rtl8192e:system/bin/hostapd_rtl8192e
+LOCAL_SRC_FILES += wpa_supplicant_rtl8192e:system/bin/wpa_supplicant
+LOCAL_SRC_FILES += hostapd_rtl8192e:system/bin/hostapd
 
 include $(WMT_PREBUILT)
 
